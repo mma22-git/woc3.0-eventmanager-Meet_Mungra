241e15b871831d04d30da58ed5ea14bac6c62255
Meet Mungra <201901050@daiict.ac.in>
2021-01-19 16:52:30 +0530

    Adding comment to learn github

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from app.models import Event,Participant
 from django.core.mail import send_mail
 import datetime
 
-# Participant
+# Participant registration
 def part(request):
     c=[]
     c1=[]
